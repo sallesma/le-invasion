b3133d65b44e53e1e83b12bd77018badbeb80aa3
Immunia <levpoubelle1@gmail.com>
2015-03-12 06:52:35 +0100

    Modifications onglets
    Plein de petits trucs j'ai modifié le code que j'ai récupéré pour faire
    que chaque nouvel onglet récupère le focus à sa création, à voir si on
    laisse. La customisation est de base et peut être changée

---
+++ src/aide_invasion_le/ClosableTabbedPane.java
@@ -0,0 +1,98 @@
+package aide_invasion_le;
+
+import java.awt.AWTKeyStroke;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.AbstractAction;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
+import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
+
+/**
+ * La classe ClosableTabbedPane extends la classe JTabbedPane permettant ainsi
+ * au JTabbedPane d'avoir un système de fermeture des onglets.
+ */
+public class ClosableTabbedPane extends JTabbedPane {
+  private static final long serialVersionUID = 1L;
+  private TabCloseUI closeUI = new TabCloseUI(this);
+  public void paint(Graphics g) {
+    super.paint(g);
+    closeUI.paint(g);
+  }
+    
+  /**
+  * On réimplémente la méthode addTab de JTabbedPane
+  */
+  @Override
+  public void addTab(String title, Component component) {
+    super.addTab(title + "  ", component);
+    this.setSelectedIndex(this.getComponentCount()-1);
+  }
+   
+  /**
+   * La methode getTabTitleAt, permet de récupérer le nom de l'onglet
+   * qui se situe à l'index donné en paramètre.
+   * @param index 
+   * @return : renvoie le nom de l'onglet qui se situe a l'index donné.
+   */
+  public String getTabTitleAt(int index) {
+    return super.getTitleAt(index).trim();
+  }
+
+  public boolean tabAboutToClose(int tabIndex) {
+    return true;
+  }
+   
+  /**
+   * La methode setupTabTraversalKeys permet d'utiliser les raccourcis : 
+   * ctrl+ tab et ctrl + shift + tab afin de naviguer entre les onglets.
+   * Elle permet également d'utiliser le raccourci ctrl+w afin de fermer l'onglet 
+   * selectionné.
+   * @param tabbedPane : le systeme d'onglets à qui on veut attribuer la methode
+   */
+  public void setupTabTraversalKeys(final JTabbedPane tabbedPane) {
+    KeyStroke ctrlTab = KeyStroke.getKeyStroke("ctrl TAB");
+    KeyStroke ctrlShiftTab = KeyStroke.getKeyStroke("ctrl shift TAB");
+    KeyStroke controlW = KeyStroke.getKeyStroke("control W");
+    /*
+     * Un Set est une collection qui n'accepte pas les doublons. Les Set sont particulièrement adaptés pour
+     * manipuler une grande quantité de données. La classe AWTKeyStroke correspond à une action sur le clavier. 
+     * Cette classe correpsond seulement à une pression ou un relachement d'une touche. (KEY_PRESSED and KEY_RELEASED)
+     * > getFocusTraversalKeys permet de récupérer les touches par défaut du composant.
+     * > on retire les touches ctrl+t et ctrl+w de la liste des touches du composant.
+     */
+    Set forwardKeys = new HashSet(tabbedPane.getFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS));
+    forwardKeys.remove(ctrlTab);
+    tabbedPane.setFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, forwardKeys);
+    Set backwardKeys = new HashSet(tabbedPane.getFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS));
+    backwardKeys.remove(ctrlShiftTab);
+    tabbedPane.setFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, backwardKeys);
+
+    /* On va alors ajouter une touche au composant JTabbedPane. InputMap fournit une liaison entre un événement 
+     * d'entrée et un objet. On utilise la classe ActionMap, afin de déterminer une action à effectuer lorsqu'
+     * une touche est enfoncée. 
+     */
+    AbstractAction closeTabAction = new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if(tabbedPane.getTabCount() > 0)
+          tabbedPane.remove(tabbedPane.getSelectedIndex());
+      }
+    };
+    /* On récupère l'entréee du composant utilisé, et on utilise ces constantes. 
+       On ajoute avec le put la liaison entre le keystroke et l'action du même nom
+       Pour le ctrl+w on ajoute la liaison à l'action anonyme.
+    */
+    InputMap inputMap = tabbedPane.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    inputMap.put(ctrlTab, "navigateNext");
+    inputMap.put(ctrlShiftTab, "navigatePrevious");
+    inputMap.put(controlW, "closeTab");
+    tabbedPane.getActionMap().put("closeTab", closeTabAction);
+  }
+}
--- src/aide_invasion_le/Fenetre.java
+++ src/aide_invasion_le/Fenetre.java
@@ -1,10 +1,20 @@
 package aide_invasion_le;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class Fenetre  extends JFrame {
@@ -14,8 +24,9 @@ public class Fenetre  extends JFrame {
 	private LEInterface leInterface;
 	private JTabbedPane tabbedPane;
 	private File mapFolder;
+	private ClosableTabbedPane p;
 	
-	public Fenetre() {
+	/*public Fenetre() {
 		JFrame fenetre = new JFrame();
 	    fenetre.setTitle("Gestionnaire Invasion");
 	    fenetre.setSize(600, 800);
@@ -37,12 +48,39 @@ public class Fenetre  extends JFrame {
 	    fenetre.add(tabbedPane);
 	    
 	    fenetre.setVisible(true);
-	}
+	}*/
+	
+	private JPanel central;
+    public Fenetre() {
+		JFrame fenetre = new JFrame();
+	    fenetre.setTitle("Gestionnaire Invasion");
+	    fenetre.setSize(600, 800);
+	    fenetre.setLocationRelativeTo(null); // Center window
+	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    leInterface = new LEInterface();
+	    mapFolder = new File("images/");
+	    
+        p = new ClosableTabbedPane();
+        p.setUI(new Tabbed());
+        MainTab mainTab = new MainTab(this, leInterface);
+        p.addTab("Main Tab", mainTab);
+        FormClassic formC= new FormClassic();
+        p.addTab("Form 1 Tab",formC);
+        FormCommando formCom= new FormCommando();
+        p.addTab("Form 2 Tab",formCom);
+        this.add(p);
+        this.setSize(600, 800);
+        this.setVisible(true);
+    }
 
 	public void openMapTab(String mapName) {
 		Path mapFile = Paths.get( this.mapFolder.getAbsolutePath(), mapName);
 		MapTab mapTab = new MapTab(leInterface, mapFile);
-	    tabbedPane.addTab(mapFile.getFileName().toString(), null, mapTab, "Map Tab");
+	    /*tabbedPane.addTab(mapFile.getFileName().toString(), null, mapTab, "Map Tab");
+	    tabbedPane.setSelectedIndex(tabbedPane.getComponentCount()-1);*/
+		p.addTab("Map Tab", mapTab);
+	    
 	}
 	
 	public File getMapFolder(){
