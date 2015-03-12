6299fda5bcb4bb5d14f83813980ea60343e206ce
Martin Salles <martin.salles@free.fr>
2015-03-12 13:24:17 +0100

    Server and pseudo are saved in a properties file

--- src/aide_invasion_le/MainTab.java
+++ src/aide_invasion_le/MainTab.java
@@ -9,8 +9,15 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -24,6 +31,7 @@ public class MainTab extends JPanel {
 
 	private Fenetre parentWindow;
 	private LEInterface leInterface;
+	private String configFile = Paths.get("data", "config.properties").toString();
 	
 	private JLabel pseudo = new JLabel("Pseudo");
 	private TextField zonePseudo = new TextField("",10);
@@ -45,6 +53,27 @@ public class MainTab extends JPanel {
 	    JPanel panel = new JPanel();
 	    panel.setLayout(new GridLayout(15, 1));
 	    
+	    if(new File(configFile).exists()) {
+	    	InputStream inputStream = null;
+	    	try {
+	    		inputStream = new FileInputStream(configFile);
+	            Properties properties = new Properties();
+	            properties.load(inputStream);
+	            zonePseudo.setText(properties.getProperty("pseudo"));
+	            zoneServer.setSelectedItem(properties.getProperty("server"));
+	        } catch(IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	        	if (inputStream != null) {
+		        	try {
+						inputStream.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+	        	}
+	        }
+	    }
+	    
 	    pseudo.setFont(new Font(pseudo.getName(), Font.PLAIN, 20));
 	    server.setFont(new Font(server.getName(), Font.PLAIN, 20));
 	    mapFolderLabel.setFont(new Font(mapFolderLabel.getName(), Font.PLAIN, 20));
@@ -52,6 +81,7 @@ public class MainTab extends JPanel {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				updateLEInterface();
+				updateConfigFile();
 			}
 			@Override
 			public void keyTyped(KeyEvent e) {}
@@ -62,6 +92,7 @@ public class MainTab extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				updateLEInterface();
+				updateConfigFile();
 			}
 		});
 		
@@ -128,6 +159,29 @@ public class MainTab extends JPanel {
 		});
 		for (File mapFile : files) {
 			this.openMapComboBox.addItem(mapFile.getName());
+		}
+	}
+
+	private void updateConfigFile()
+	{
+		Properties properties = new Properties();
+		OutputStream outputStream = null;
+
+		try {
+			outputStream = new FileOutputStream(configFile);
+			properties.setProperty("pseudo", zonePseudo.getText());
+			properties.setProperty("server", zoneServer.getSelectedItem().toString());
+			properties.store(outputStream, null);
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
